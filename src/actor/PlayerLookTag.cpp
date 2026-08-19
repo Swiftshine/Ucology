4cccb83ec5060aa7f170748e6464e8e91ff0379c
Swift <swiftshineexists@gmail.com>
2026-08-19 12:37:05 -0500

    finish PlayerLookTag

--- src/actor/PlayerLookTag.cpp
+++ src/actor/PlayerLookTag.cpp
@@ -1,6 +1,3 @@
-// TODO: movement-controlled look target
-// TODO: event-controlled look target (look when target is activated, etc.)
-
 #include <actor/Actor.h>
 #include <actor/Profile.h>
 #include <actor/AttentionLookat.h>
@@ -11,15 +8,16 @@
 #include <player/PlayerMgr.h>
 #include <player/PlayerObject.h>
 #include <red/util/SpriteUtil.h>
+#include <map/SwitchFlagMgr.h>
 
 namespace ucology {
 
 class PlayerLookTag : public Actor {
 private:
     enum LookAt : u8 {
-        Position,
-        PositionWhileInScreen,
-        Screen
+        PositionAlways,
+        PositionWhileInLocation,
+        ScreenWhileInLocation
     };
 public:
     static Profile* cProfile;
@@ -31,24 +29,53 @@ public:
     Result create() override;
     bool execute() override;
 
-    void takePlayerAttention() {
+    void takePlayerAttention() const {
         AttentionMgr::instance()->entry(mPlayerAttention);
     }
 
-    void updatePlayerAttention() {
+    void updateAttentionPos() {
         sead::Vector2f& pos = mPlayerAttention.getPos();
         pos.x = mPos.x;
         pos.y = mPos.y;
     }
 
-    void releasePlayerAttention() {
+    void releasePlayerAttention() const {
         AttentionMgr::instance()->release(mPlayerAttention);
     }
+
+    void releasePlayerAttentionIfTaken() {
+        if (mHasPlayerAttention) {
+            releasePlayerAttention();
+            mHasPlayerAttention = false;
+        }
+    }
+
+    bool isEventActive() const {
+        return mEventID != 0 ? SwitchFlagMgr::instance()->isActivated(mEventID - 1) : true;
+    }
+
+    sead::BoundBox2f getLocationBounds() const {
+        sead::BoundBox2f bounds;
+        const CourseDataFile* area = CourseData::instance()->getFile(CourseInfo::instance()->getFileNo());
+        const Location* location = area->getLocation(&bounds, mTargetLocationID);
+
+        if (location == nullptr) {
+            tk::fatal("PlayerLookTag - the specified location (id %d) does not exist", mTargetLocationID);
+            return bounds;
+        }
+
+        return bounds;
+    }
+
+    void update_LookAt_PositionAlways();
+    void update_LookAt_PositionWhileInLocation();
+    void update_LookAt_ScreenWhileInLocation();
 private:
     LookAt mPlayerLookType;
     AttentionLookat mPlayerAttention;
     u8 mTargetLocationID;
     bool mHasPlayerAttention;
+    u8 mEventID;
 };
 
 const ActorCreateInfo PlayerLookTag::cCreateInfo = {
@@ -67,69 +94,76 @@ PlayerLookTag::PlayerLookTag(const ActorCreateParam& param)
 ActorBase::Result PlayerLookTag::create() {
     mPlayerLookType = static_cast<LookAt>(red::SpriteUtil::getNybble1(this));
     mTargetLocationID = static_cast<u8>(red::SpriteUtil::getNybbleRange(this, 2, 3));
-
-    switch (mPlayerLookType) {
-        case LookAt::Position: {
-            takePlayerAttention();
-            updatePlayerAttention();
-            mHasPlayerAttention = true;
-            break;
-        }
-
-        case LookAt::PositionWhileInScreen:
-        case LookAt::Screen: {
-            mHasPlayerAttention = false;
-            if (mTargetLocationID == 0) {
-                tk::fatal("PlayerLookTag - Must set a non-zero location ID to make the player look at the target");
-                return cResult_Failed;
-            }
-        }
-    }
+    mEventID = static_cast<u8>(red::SpriteUtil::getNybbleRange(this, 4, 5));
+    mHasPlayerAttention = false;
 
     return cResult_Success;
 }
 
 bool PlayerLookTag::execute() {
-    if (mPlayerLookType == LookAt::Position || mPlayerLookType == LookAt::PositionWhileInScreen) {
-        updatePlayerAttention();
-        if (mPlayerLookType == LookAt::Position) {
-            return true;
-        }
+    switch (mPlayerLookType) {
+        case LookAt::PositionAlways: update_LookAt_PositionAlways(); break;
+        case LookAt::PositionWhileInLocation: update_LookAt_PositionWhileInLocation(); break;
+        case LookAt::ScreenWhileInLocation: update_LookAt_ScreenWhileInLocation(); break;
     }
+    return true;
+}
 
-    for (s32 i = 0; i < PlayerMgr::instance()->getNum(); i++) {
-        PlayerObject* player = PlayerMgr::instance()->getPlayerObject(i);
-        
-        if (player == nullptr) {
-            continue;
-        }
-        
-        // check if player is within a location
-        sead::BoundBox2f locationBounds;
-        const CourseDataFile* area = CourseData::instance()->getFile(CourseInfo::instance()->getFileNo());
-        const Location* location = area->getLocation(&locationBounds, mTargetLocationID);
-        if (location == nullptr) {
-            tk::fatal("PlayerLookTag - the specified location %d does not exist", mTargetLocationID);
-            return false;
-        }
+void PlayerLookTag::update_LookAt_PositionAlways() {
+    updateAttentionPos();
 
-        if (locationBounds.isInside(player->getPos2D())) {
-            if (mPlayerLookType == LookAt::Screen) {
-                // look at the screen
-                player->setClampFaceRot();
-            } else if (mPlayerLookType == LookAt::PositionWhileInScreen) {
+    if (isEventActive()) {
+        if (!mHasPlayerAttention) {
+            takePlayerAttention();
+            mHasPlayerAttention = true;
+        }
+    } else {
+        releasePlayerAttentionIfTaken();
+    }
+}
+
+void PlayerLookTag::update_LookAt_PositionWhileInLocation() {
+    updateAttentionPos();
+
+    if (isEventActive()) {
+        for (s32 i = 0; i < PlayerMgr::instance()->getNum(); i++) {
+            PlayerObject* player = PlayerMgr::instance()->getPlayerObject(i);
+
+            if (player == nullptr) {
+                continue;
+            }
+
+            // check if player is within bounds
+            if (getLocationBounds().isInside(player->getPos2D())) {
                 if (!mHasPlayerAttention) {
                     takePlayerAttention();
-                    mHasPlayerAttention = true;
+                    mHasPlayerAttention = true;   
                 }
+            } else {
+                releasePlayerAttentionIfTaken();
             }
-        } else if (mPlayerLookType == LookAt::PositionWhileInScreen && mHasPlayerAttention) {
-            releasePlayerAttention();
-            mHasPlayerAttention = false;
+        }
+    } else {
+        releasePlayerAttentionIfTaken();
+    }
+}
+
+void PlayerLookTag::update_LookAt_ScreenWhileInLocation() {
+    if (isEventActive()) {
+        for (s32 i = 0; i < PlayerMgr::instance()->getNum(); i++) {
+            PlayerObject* player = PlayerMgr::instance()->getPlayerObject(i);
+
+            if (player == nullptr) {
+                continue;
+            }
+
+            // check if player is within bounds
+            if (getLocationBounds().isInside(player->getPos2D())) {
+                // look at the screen
+                player->setClampFaceRot();
+            }
         }
     }
-    
-    return true;
 }
 
 }
